0ddef42f38ab14d02cf5e02150a2467f08b5389a
Manobal-Singh-Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2024-03-09 02:57:59 +0530

    add auth to testRoute

--- backend/src/routers/routes/testRoute.ts
+++ backend/src/routers/routes/testRoute.ts
@@ -1,12 +1,22 @@
-import {Router} from "express";
+import {NextFunction, Request, Response, Router} from "express";
 import {PrismaClient} from "@prisma/client";
+import {ClerkExpressRequireAuth} from "@clerk/clerk-sdk-node";
+import {ReasonPhrases, StatusCodes} from "http-status-codes";
+import app from "@src/server";
 
 const testRouter = Router();
 
 const prisma = new PrismaClient();
-testRouter.get('/', async (_req, res, next) => {
+testRouter.get('/', ClerkExpressRequireAuth(), async (_req, res, next) => {
     const users = await prisma.user.findMany();
-    res.send(users);
+    res.json(users);
 })
 
+testRouter.use((err: Error, _: Request, res: Response, next: NextFunction) => {
+    console.error(err.stack);
+    res.status(StatusCodes.UNAUTHORIZED).json({
+        error: ReasonPhrases.UNAUTHORIZED,
+        message: 'You must be authenticated to access this resource.'
+    });
+});
 export default testRouter;
