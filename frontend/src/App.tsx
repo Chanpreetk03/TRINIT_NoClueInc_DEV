76bbfc188c9e260ae28593e4f3907f996211174f
Chanpreet Kaur <6kchanpreet@gmail.com>
2024-03-08 20:35:52 +0530

    set up frontend react ts app with react router

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import App from './App'; // Your main app component
+
+const Root = () => {
+  return (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+};
+
+export default Root;
